83b6c1d857043697022173d3850f49201d4cd013
TommyB-DontMindMe <142879081+TommyB-DontMindMe@users.noreply.github.com>
2025-01-24 22:11:23 +0100

    Implemented basic UI framework

---
+++ Source/Beyond_The_Bubble/Private/Core/BB_WidgetComponent.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/BB_WidgetComponent.h"
+#include "UI/BaseWidget.h"
+
+// Sets default values for this component's properties
+UBB_WidgetComponent::UBB_WidgetComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UBB_WidgetComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	if (!CurrentPlayer)
+	{
+		CurrentPlayer = GetWorld()->GetFirstPlayerController();
+	}
+
+	if (!BaseWidgetInstance)
+	{
+		BaseWidgetInstance = CreateWidget<UBaseWidget>(CurrentPlayer, WidgetBaseClass);
+		if (BaseWidgetInstance)
+			BaseWidgetInstance->AddToViewport();
+	}
+	
+}
+
+
+// Called every frame
+void UBB_WidgetComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/Beyond_The_Bubble/Public/UI/BaseWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BaseWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEYOND_THE_BUBBLE_API UBaseWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
